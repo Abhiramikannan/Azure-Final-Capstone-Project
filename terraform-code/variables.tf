a309733b3f44885d7b13c9dd47325a5468501c42
Abhirami kannan <85607268+Abhiramikannan@users.noreply.github.com>
2025-06-19 18:03:18 +0530

    Create variables.tf

---
+++ terraform-code/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type        = string
+  default     = "abhi-resource-group"
+}
+
+variable "location" {
+  type        = string
+  default     = "Central India"
+}
+
+variable "vnet1_location" {
+  type        = string
+  default     = "Central India"
+}
+
+variable "vnet2_location" {
+  type        = string
+  default     = "West Europe"
+}
