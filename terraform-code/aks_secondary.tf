16704e6980e1918b61e8016bf0777f805ef97e63
Abhirami kannan <85607268+Abhiramikannan@users.noreply.github.com>
2025-06-19 18:13:03 +0530

    Update aks_secondary.tf

--- terraform-code/aks_secondary.tf
+++ terraform-code/aks_secondary.tf
@@ -3,25 +3,27 @@ resource "azurerm_kubernetes_cluster" "aks2" {
   location            = var.vnet2_location
   resource_group_name = var.resource_group_name
   dns_prefix          = "abhiaks2"
-
+ 
   default_node_pool {
     name           = "secondarynp1"
     node_count     = 1
     vm_size        = "Standard_B2s"
     vnet_subnet_id = azurerm_subnet.vnet2_subnet1.id
   }
-
+ 
   identity {
     type = "SystemAssigned"
   }
-
+ 
   network_profile {
     network_plugin    = "azure"
     load_balancer_sku = "standard"
     network_policy    = "azure"
   }
+ 
+  depends_on = [azurerm_subnet.vnet2_subnet1]
 }
-
+ 
 resource "azurerm_kubernetes_cluster_node_pool" "usernp2" {
   name                  = "secondarynp2"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks2.id
@@ -30,4 +32,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "usernp2" {
   mode                  = "User"
   vnet_subnet_id        = azurerm_subnet.vnet2_subnet2.id
   orchestrator_version  = azurerm_kubernetes_cluster.aks2.kubernetes_version
+ 
+  depends_on = [azurerm_subnet.vnet2_subnet2]
 }
+ 
