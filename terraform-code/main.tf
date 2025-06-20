70169d0cd234d4f11e0a49a2caef66a656bf32d1
Abhirami kannan <85607268+Abhiramikannan@users.noreply.github.com>
2025-06-20 17:58:39 +0530

    Update main.tf

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -19,6 +19,8 @@ terraform {
 provider "azurerm" {
   features {}
 }
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "myrg" {
   name     = var.resource_group_name
   location = var.location
